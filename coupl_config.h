fb7259359bb5d7af40ac4888ca29aee3a4665432
Humberto Jr <hsilva@br006.pvt.bridges.psc.edu>
2020-01-14 19:04:56 -0500

    Adding CMATRIX_DATAFILE_FORMAT macro

--- coupl_config.h
+++ coupl_config.h
@@ -4,4 +4,8 @@
 	#if !defined(CMATRIX_BUFFER_FORMAT)
 		#define CMATRIX_BUFFER_FORMAT "cmatrix_arrang=%c_n=%d_J=%d.bin"
 	#endif
+
+	#if !defined(CMATRIX_DATAFILE_FORMAT)
+		#define CMATRIX_DATAFILE_FORMAT "cmatrix_arrang=%c_n=%d_J=%d.dat"
+	#endif
 #endif
