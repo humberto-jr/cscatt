d2c0d3d4b0d9165ad5a752518dee8be054915513
Humberto Jr <hsilva@br018.pvt.bridges.psc.edu>
2020-01-23 22:06:18 -0500

    Adding new features to coupl_config.h header

--- coupl_config.h
+++ coupl_config.h
@@ -8,4 +8,50 @@
 	#if !defined(CMATRIX_DATAFILE_FORMAT)
 		#define CMATRIX_DATAFILE_FORMAT "cmatrix_arrang=%c_n=%d_J=%d.dat"
 	#endif
+
+	/******************************************************************************
+
+	 Function check_coupl(): checks whether the n-th coupling matrix file exist in
+	 the disk for a given arrangement and J.
+
+	******************************************************************************/
+
+	inline static bool check_coupl(const char arrang, const int n, const int J)
+	{
+		char filename[MAX_LINE_LENGTH];
+		sprintf(filename, CMATRIX_BUFFER_FORMAT, arrang, n, J);
+
+		return file_exist(filename);
+	}
+
+	/******************************************************************************
+
+	 Function count_coupl(): counts how many coupling matrices are available in the
+	 disk for a given arrangement and J.
+
+	******************************************************************************/
+
+	inline static int count_coupl(const char arrang, const int J)
+	{
+		int counter = 0;
+		while (check_coupl(arrang, counter, J)) ++counter;
+
+		return counter;
+	}
+
+	/******************************************************************************
+
+	 Function load_coupl(): load from the disk the n-th coupling matrix for a given
+	 arrangement and J.
+
+	******************************************************************************/
+
+	inline static void load_coupl(const char arrang,
+	                              const int n, const int J, matrix *c)
+	{
+		char filename[MAX_LINE_LENGTH];
+		sprintf(filename, CMATRIX_BUFFER_FORMAT, arrang, n, J);
+
+		c = matrix_load(filename);
+	}
 #endif
