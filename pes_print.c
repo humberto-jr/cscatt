1b9a6addfcfcbab12371976ab88e5c0498662a38
humberto <dogmacentral@hotmail.com>
2021-01-01 23:37:57 -0800

    Refactoring the pes_print driver

--- pes_print.c
+++ pes_print.c
@@ -2,11 +2,28 @@
 #include "modules/file.h"
 #include "modules/globals.h"
 
-#define FORMAT "% 6f\t % 6f\t % 6f\t % -8e % -8e % -8e\n"
+#define SINGLE_FORMAT "% 6f\t % -8e % -8e % -8e\n"
+
+#define DOUBLE_FORMAT "% 6f\t % 6f\t % -8e % -8e % -8e\n"
+
+#define FULL_FORMAT "% 6f\t % 6f\t % 6f\t % -8e % -8e % -8e\n"
+
+void get_pes(const double r,
+             const double R,
+             const double theta,
+             const double shift,
+             const double scale,
+             double *a, double *b, double *c)
+{
+	*a = (pes_abc('a', r, R, theta) + shift)*scale;
+	*b = (pes_abc('b', r, R, theta) + shift)*scale;
+	*c = (pes_abc('c', r, R, theta) + shift)*scale;
+}
 
 int main(int argc, char *argv[])
 {
 	ASSERT(argc > 1)
+
 	file_init_stdin(argv[1]);
 
 /*
@@ -70,38 +87,145 @@ int main(int argc, char *argv[])
 	printf("# r     = (%f, %f, %f)\n", r_min, r_max, r_step);
 	printf("# R     = (%f, %f, %f)\n", R_min, R_max, R_step);
 	printf("# theta = (%f, %f, %f)\n", theta_min, theta_max, theta_step);
-	printf("# Shift = %f\n", shift);
-	printf("# Scale = %f\n", scale);
+	printf("# shift = %f\n", shift);
+	printf("# scale = %f\n", scale);
 
 	printf("#\n");
 
-	for (int n = 0; n < rovib_grid_size; ++n)
+	double a = 0.0, b = 0.0, c = 0.0;
+
+	if (r_step == 0.0 && R_step == 0.0 && theta_step > 0.0)
 	{
-		const double r = r_min + as_double(n)*r_step;
+		const double r = r_min;
+		const double R = R_min;
 
-		for (int m = 0; m < scatt_grid_size; ++m)
+		for (int n = 0; n <= theta_grid_size; ++n)
 		{
-			const double R = R_min + as_double(m)*R_step;
+			const double theta = theta_min + as_double(n)*theta_step;
 
-			for (int p = 0; p <= theta_grid_size; ++p)
+			get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+			printf(SINGLE_FORMAT, theta, a, b, c);
+		}
+	}
+
+	else if (r_step == 0.0 && R_step > 0.0 && theta_step == 0.0)
+	{
+		const double r = r_min;
+		const double theta = theta_min;
+
+		for (int n = 0; n < scatt_grid_size; ++n)
+		{
+			const double R = R_min + as_double(n)*R_step;
+
+			get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+			printf(SINGLE_FORMAT, R, a, b, c);
+		}
+	}
+
+	else if (r_step > 0.0 && R_step == 0.0 && theta_step == 0.0)
+	{
+		const double R = R_min;
+		const double theta = theta_min;
+
+		for (int n = 0; n < rovib_grid_size; ++n)
+		{
+			const double r = r_min + as_double(n)*r_step;
+
+			get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+			printf(SINGLE_FORMAT, r, a, b, c);
+		}
+	}
+
+	else if (r_step == 0.0 && R_step > 0.0 && theta_step > 0.0)
+	{
+		const double r = r_min;
+
+		for (int n = 0; n < scatt_grid_size; ++n)
+		{
+			const double R = R_min + as_double(n)*R_step;
+
+			for (int m = 0; m <= theta_grid_size; ++m)
 			{
-				const double theta = theta_min + as_double(p)*theta_step;
+				const double theta = theta_min + as_double(m)*theta_step;
 
-				const double a = (pes_abc('a', r, R, theta) + shift)*scale;
-				const double b = (pes_abc('b', r, R, theta) + shift)*scale;
-				const double c = (pes_abc('c', r, R, theta) + shift)*scale;
+				get_pes(r, R, theta, shift, scale, &a, &b, &c);
 
-				printf(FORMAT, r, R, theta, a, b, c);
-
-				if (p == 0 && theta_step == 0.0) break;
+				printf(DOUBLE_FORMAT, R, theta, a, b, c);
 			}
 
-			if (rovib_grid_size > 1 && scatt_grid_size > 1 && theta_step != 0.0) printf("\n");
-			if (m == 0 && R_step == 0.0) break;
+			printf("\n");
 		}
+	}
 
-		if (scatt_grid_size > 1 && R_step != 0.0) printf("\n");
-		if (n == 0 && r_step == 0.0) break;
+	else if (r_step > 0.0 && R_step == 0.0 && theta_step > 0.0)
+	{
+		const double R = R_min;
+
+		for (int n = 0; n < rovib_grid_size; ++n)
+		{
+			const double r = r_min + as_double(n)*r_step;
+
+			for (int m = 0; m <= theta_grid_size; ++m)
+			{
+				const double theta = theta_min + as_double(m)*theta_step;
+
+				get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+				printf(DOUBLE_FORMAT, r, theta, a, b, c);
+			}
+
+			printf("\n");
+		}
+	}
+
+	else if (r_step > 0.0 && R_step > 0.0 && theta_step == 0.0)
+	{
+		const double theta = theta_min;
+
+		for (int n = 0; n < rovib_grid_size; ++n)
+		{
+			const double r = r_min + as_double(n)*r_step;
+
+			for (int m = 0; m <= scatt_grid_size; ++m)
+			{
+				const double R = R_min + as_double(m)*R_step;
+
+				get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+				printf(DOUBLE_FORMAT, r, R, a, b, c);
+			}
+
+			printf("\n");
+		}
+	}
+
+	else
+	{
+		for (int n = 0; n < rovib_grid_size; ++n)
+		{
+			const double r = r_min + as_double(n)*r_step;
+
+			for (int m = 0; m < scatt_grid_size; ++m)
+			{
+				const double R = R_min + as_double(m)*R_step;
+
+				for (int p = 0; p <= theta_grid_size; ++p)
+				{
+					const double theta = theta_min + as_double(p)*theta_step;
+
+					get_pes(r, R, theta, shift, scale, &a, &b, &c);
+
+					printf(FULL_FORMAT, r, R, theta, a, b, c);
+				}
+
+				printf("\n");
+			}
+
+			printf("\n");
+		}
 	}
 
 	return EXIT_SUCCESS;
